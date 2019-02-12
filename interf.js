cb1a6351c23838efcd09c6251c04072df3e0b354
cc1539 <43189993+cc1539@users.noreply.github.com>
2019-02-11 22:22:16 -0500

    Add files via upload

--- interf.js
+++ interf.js
@@ -21,6 +21,36 @@ window.onload = function() {
 		elem.nextElementSibling.style.display = "block";
 		//elem.onclick = new Function('toggleCollapsible(this)');
 	});
+	
+	var projdiv = document.getElementById("projdiv");
+	if(projdiv) {
+		// then use ajax to load data
+		$.ajax({
+			url: "data.json",
+			success: function(response) {
+				/*
+					<p class="collapsible">+ Simple Euler Fluid</p>
+						<div>
+							<hr>
+							
+							<p><b>Description</b></p>
+							<p class="desc">2D euler (grid-based) fluid simulation. The SFML library is used for the window-handling and graphics.</p><hr>
+							
+							<div class="imgcontain">
+								<img src="assets/images/eulerfluid.png"></a>
+								<div class="gitlink">
+									<a href="https://github.com/Metashian/-sfml-euler-fluid-test" target="_blank"></a>
+								</div>
+							</div>
+							
+							<hr>
+							
+						</div>
+					*/
+			}
+		});
+	}
+	
 	/*
 	setTimeout(function(){
 		alert("Hi, I am an Albanian virus but because of poor technology in my country unfortunately I am not able to harm your computer. Please be so kind to delete one of your important files yourself and then forward me to other users. Many thanks for your cooperation! Best regards,Albanian virus");
