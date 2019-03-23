67505122cbff2e0ddc168704946de35fcd01bff5
cc1539 <43189993+cc1539@users.noreply.github.com>
2019-03-22 21:38:39 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,59 @@
+
+function buildContent(data) {
+	for(let key in data) {
+		
+		let name = data[key].link_name;
+		let content = data[key].content;
+		
+		let section = $("<div id='"+key+"' class='entry'></div>");
+		
+		let section_link = $("<p>"+name+"</p>");
+		
+		section_link.click(function(){
+			$("html,body").animate({scrollTop:section.offset().top-100});
+		});
+		
+		$("#header").prepend(section_link);
+		
+		section.append("<h1>"+name+"</h1>");
+		
+		if(typeof content === 'string') {
+			section.append("<p>"+content+"</p>");
+		} else {
+			// it's a dictionary
+			content.forEach(function(entry){
+				let part = $("<div></div>");
+				for(let prop in entry) {
+					if(prop=='title') {
+						part.append("<p class='title'>"+entry[prop]+"</p>");
+					} else if(prop=='content' || prop=='desc') {
+						part.append("<p class='desc'>"+entry[prop]+"</p>");
+					} else if(prop=='link') {
+						if(key=="projects") {
+							if(entry[prop]!="N/A") {
+								var container = $("<div class='git-link'></div>");
+								container.append("<img src='assets/images/"+entry["img"]+"'>");
+								container.append("<div><img src='assets/images/git-link.png'></div>");
+								part.append(container);
+								container.click(function(){
+									location.href = entry[prop];
+								});
+							}
+						} else if(key=="social") {
+							part.append("<a href='"+entry["link"]+"' target='_blank'><img src='assets/images/"+entry["img"]+"' title='"+entry["tag"]+"'></a>");
+						} else {
+							part.append("<a href='"+entry[prop]+"' target='_blank'>Link</p>");
+						}
+					}
+				}
+				section.append(part);
+				if(key!="social") {
+					section.append("<hr>");
+				}
+			});
+		}
+		$("#content").append(section);
+	}
+}
+
+$(function(){$.ajax({url:"content/data.json",success:buildContent});});
